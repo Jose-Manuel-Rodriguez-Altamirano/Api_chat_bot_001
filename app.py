c293305e414ef053e34ff17a2156f6c510b310e0
José Manuel Rodríguez Altamirano <omegaodin34@gmail.com>
2024-04-03 17:38:04 +0200

    Main-Proyect

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from flask_cors import CORS
+
+import generate_answer
+
+app = Flask(__name__)
+CORS(app, methods=[ 'POST', 'GET' ], allow_headers=[ 'Content-Type' ])
+
+@app.route('/', methods=['GET'])
+def index():
+    return {
+        "answer": "success"
+    }, 200
+
+@app.route('/get_answer', methods=['POST'])
+def get_answer():
+    data = request.get_json()
+    question = data["question"]
+    message = generate_answer.get_response_from_ai(question)
+
+    print(message)
+    return {
+        "answer": message
+    }, 200
